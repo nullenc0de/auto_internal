b52f1163c5b955a880a2eb947fb773029b786b58
nullenc0de <pseekamp@gmail.com>
2019-12-05 13:53:48 -0500

    Update auto_recon.sh

--- bin/auto_recon.sh
+++ bin/auto_recon.sh
@@ -9,7 +9,7 @@ echo "Enter Password (i.e. Password123!)"
 read pass
 echo "Enter Domain (i.e. Username Prefix)"
 read domain
-
+#change dirs
 cd logs
 #Dumps DNS records
 echo "Dumping DNS records."
@@ -19,17 +19,14 @@ echo
 echo "Dumping AD Objects."
 ldapdomaindump -u $domain\\$user -p "$pass" $dc
 echo
-
 #updates targets with new hosts
 cat records.csv |cut -d "," -f3 |sort -u |grep -E '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)' > ../hosts/targets.txt
 echo
-
 #Printing out external DNS records
 cat records.csv |cut -d "," -f3 |sort -u |grep -vE '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)' |grep -v "ip"
 echo "These external records displayed might be juicy :)"
 echo "Careful, they might be out of scope"
 echo
-
 #Output hosts and users to files.
 cat domain_computers.json |grep -A1 dNSHostName | cut -d '"' -f2 |grep -v "dNSHostName" |grep -v '-' >> ../hosts/targets.txt
 cat domain_users.json |grep -A1 sAMAccountName | cut -d '"' -f2 |grep -v "sAMAccountName" |sort -u > AD_users.txt
