4e9b0f3482c1f30fe1d79ee351118a36fd69349f
nullenc0de <pseekamp@gmail.com>
2019-12-05 13:20:57 -0500

    Update auto_recon.sh

--- bin/auto_recon.sh
+++ bin/auto_recon.sh
@@ -19,7 +19,7 @@ ldapdomaindump -u $domain\\$user -p "$pass" $dc
 echo
 
 #updates targets with new hosts
-cat records.csv |cut -d "," -f3 |sort -u |grep -E '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)' > hosts/targets.txt
+cat records.csv |cut -d "," -f3 |sort -u |grep -E '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)' > ../hosts/targets.txt
 echo
 
 #Printing out external DNS records
@@ -29,8 +29,8 @@ echo "Careful, they might be out of scope"
 echo
 
 #Output hosts and users to files.
-cat domain_computers.json |grep -A1 dNSHostName | cut -d '"' -f2 |grep -v "dNSHostName" |grep -v '-' >> hosts/targets.txt
-cat domain_users.json |grep -A1 sAMAccountName | cut -d '"' -f2 |grep -v "sAMAccountName" |sort -u > logs/AD_users.txt
+cat domain_computers.json |grep -A1 dNSHostName | cut -d '"' -f2 |grep -v "dNSHostName" |grep -v '-' >> ../hosts/targets.txt
+cat domain_users.json |grep -A1 sAMAccountName | cut -d '"' -f2 |grep -v "sAMAccountName" |sort -u > AD_users.txt
 echo 
-echo $(cat logs/AD_users.txt |wc -l) "Users found in logs/AD_users.txt"
-echo $(cat hosts/targets.txt |wc -l) "Hosts found in hosts/targets.txt"
+echo $(cat AD_users.txt |wc -l) "Users found in logs/AD_users.txt"
+echo $(cat ../hosts/targets.txt |wc -l) "Hosts found in hosts/targets.txt"
